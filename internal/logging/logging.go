1224dd7c16a82e7242595f6edfdc84115080c493
buengese <buengese@protonmail.com>
2023-04-29 01:03:50 +0200

    refactor: move logging to internal

--- internal/logging/logging.go
+++ internal/logging/logging.go
@@ -1,3 +1,5 @@
+// Copyright 2023 Sebastian Bünger
+// SPDX-License-Identifier: AGPL-3.0-only OR MIT
 package logging
 
 import (
@@ -10,35 +12,30 @@ import (
 	"github.com/rs/zerolog/pkgerrors"
 )
 
-const (
-	// FileMaxSize is the maximum size of the log file in MB.
-	FileMaxSize = 5
-	// FileMaxAge is the maximum age of the log file in days.
-	FileMaxAge = 14
-)
-
 //nolint:gochecknoglobals // Globals are ok for logging imho.
 var (
 	once   sync.Once
 	logger zerolog.Logger
 )
 
-func Setup() {
+// GetLogger returns a logger instance.
+func GetLogger() *zerolog.Logger {
 	once.Do(func() {
 		zerolog.TimeFieldFormat = time.RFC3339Nano
 		zerolog.ErrorStackMarshaler = pkgerrors.MarshalStack //nolint:reassign // Same here.
 
 		var output io.Writer = zerolog.ConsoleWriter{
-			Out:        os.Stderr,
+			Out:        os.Stdout,
 			TimeFormat: time.RFC3339,
 		}
 
 		logger = zerolog.New(output).
-			Level(zerolog.DebugLevel).
+			Level(zerolog.TraceLevel).
 			With().
 			Timestamp().
 			Logger()
 
 		zerolog.DefaultContextLogger = &logger
 	})
+	return &logger
 }
