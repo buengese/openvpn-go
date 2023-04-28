877481151b124d8a459eb6b0b98d77914a971958
buengese <buengese@protonmail.com>
2023-04-29 01:02:16 +0200

    feat: simplify shell command handling code

--- runner/runner.go
+++
@@ -1,6 +0,0 @@
-package runner
-
-type Runner interface {
-	// Run runs command with given arguments and returns stdout, stderr and error
-	Run(arguments []string) (stdout string, stderr string, err error)
-}
